9c48d9f7d0c3c1135d8652c5a9d41d802eca9319
YourBrother <yourbr0ther.tv@gmail.com>
2023-04-20 00:29:48 -0400

    Updated some more files/folders

---
+++ Languages/Nodejs/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+console.log(process.env.OPENAI_API_KEY)
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+//const openai = new OpenAIApi(configuration);
+
+//const completion = openai.createChatCompletion({
+ // model: "gpt-3.5-turbo",
+  //messages: [{role: "user", content: "Hello world"}],
+//});
+
