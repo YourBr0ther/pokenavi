3a1b6c216b9769ac2804c3c9dafff0bce3b746a3
YourBrother <yourbr0ther.tv@gmail.com>
2023-04-22 14:13:27 -0400

    manually created successful primeChatBot function

--- new-server.js
+++ new-server.js
@@ -1,9 +1,52 @@
+/*
+
+Start program
+Load the first JSON file
+Feed the selected JSON file to OpenAI to prime the chatbot
+Put the response into index 0 of the message array
+
+WAIT
+
+User inputs first request.
+The request is passed to the OpenAI with the message array and saved to the message array
+The chatbot returns the response and saved to the message array
+
+Import Values
+ * JSON Index
+ * Messages
+ * JSON Personality Sheet
+ * Species Number
+
+
+
+
+
+
+
+
+Button press
+
+Switch
+The switch button will cause the selected JSON to be changed out for the next one in the JSON directory.
+The new JSON file will be sent to OpenAI to prim the chatbot again
+The message array will be purge
+
+
+
+
+
+
+
+
+
+*/
+
 // Clear the console
 console.clear()
 
 //  * Packages 
-// Env module
-require('dotenv').config
+// ENV module
+require('dotenv').config();
 // OpenAI modules
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
@@ -15,48 +58,63 @@ const bodyParser = require("body-parser");
 const fs = require('fs');
 const path = require('path');
 const util = require("util");
+const { domainToASCII } = require('url');
 
 // * Constant Variables
 // JSON directory
 const directoryPath = path.join(__dirname, './JSON/');
-console.log('JSON Files: ' + directoryPath)
+console.log('JSON Directory: ' + directoryPath)
 // JSON Files
 const jsonFileNames = fs.readdirSync(directoryPath).filter(file => path.extname(file) === '.json');
 console.log('JSON Files: ' + jsonFileNames)
 
 // Import first JSON
 let JSONIndex = 0
-let activePokemon = async (JSONIndex) => {
+let getPokemon = async (JSONIndex) => {
     let testPath = `${directoryPath}/${jsonFileNames[JSONIndex]}`
-    let selectedJSON
-    fs.readFile(testPath, (err, data) => {
-        if (err) {
-            console.log(err)
-            return
-        }
-    
-        try {
-            selectedJSON = JSON.parse(data)
-            console.log(activeJSON)
-        } catch (err) {
-            console.error(err)
-            return
-        }
-    })
+    let selectedPokemon
+    const readFile = util.promisify(fs.readFile)
+    try {
+        const data = await readFile(testPath)
+        selectedPokemon = JSON.parse(data)
+    } catch (err) {
+        console.err('Error importing the JSON file: ' + err)
+    }
 
-
-    return selectedJSON
+    return selectedPokemon
 }
-// Import first JSON
 
+let selectedPokemon
+let messages = [];
+//let activeJSON
+
+// Import first JSON
 (async () => {
-    const selectedJSON = await activePokemon(JSONIndex);
-    console.log(selectedJSON);
+    selectedPokemon = await getPokemon(JSONIndex);
+    console.log('Name: ' + selectedPokemon.Nickname);
+    console.log('National Dex: ' + selectedPokemon.NationalPokedexNumber);
+    primeChatBot(selectedPokemon)
 })();
 
+async function primeChatBot(selectedPokemon) {
+    let response
+    console.log('Pokemon for priming: ' + selectedPokemon.Nickname)
+    const pkmnSheet = selectedPokemon.PersonalitySheet
+    messages.push({ role: "system", content: pkmnSheet })
+
+    try {
+        response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+            temperature: 0.7
+        });
+    } catch (error) {
+        console.error(error)
+    }
+    console.log(response)
+}
 
 // * Dynamic Variables
 // Short-term Memory
-let messages = [];
 
-let activeJSON
+
