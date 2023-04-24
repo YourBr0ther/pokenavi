287269d132f077dbaf0261484802acad3df85f00
YourBr0ther <yourbr0ther.tv@gmail.com>
2023-04-24 14:10:29 -0400

    Working prompt and mod history now

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const jsonFileNames = fs.readdirSync(directoryPath).filter(file => path.extname(
 console.log('JSON Files: ' + jsonFileNames)
 console.log('')
 // Message Logs for Each Pokemon
-const messageLogs = {}
+const runningMemoryLogs = {}
+const interactionHistoryLogs = {}
 // How long to keep the conversations
 const conversationMemoryDuration = 7 * 24 * 60 * 60 * 1000
 
@@ -71,28 +72,31 @@ async function primeChatBot(selectedPokemon) {
     // Pull the string from the Pokemon JSON
     const pkmnSheet = selectedPokemon.PersonalitySheet
 
-    // Push the pkmnSheet to the message array
-    messageLogs[selectedPokemon.NationalPokedexNumber].push({ role: "system", content: pkmnSheet, timestamp: new Date().toISOString() })
+    runningMemoryLogs[selectedPokemon.NationalPokedexNumber].push({ role: "system", content: pkmnSheet, timestamp: new Date().toISOString() });
+
+    // Create a temporary array called primeRunningMemory
+    const primeRunningMemory = runningMemoryLogs[selectedPokemon.NationalPokedexNumber].slice();
+
+    // Push the pkmnSheet to the primeRunningMemory array
+    primeRunningMemory.push({
+        role: "system",
+        content: pkmnSheet,
+        timestamp: new Date().toISOString(),
+    });
 
     // Send pkmnSheet via the message array to ChatGPT and put response in response variable
     try {
-        response = await openai.createChatCompletion({
+         response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
-            messages: messageLogs[selectedPokemon.NationalPokedexNumber].map(({ role, content }) => ({ role, content })), // Only send messages for this Pokemon
+            messages: primeRunningMemory.map(({ role, content }) => ({ role, content })), // Only send messages for this Pokemon
             temperature: 0.7,
+            max_tokens: 1
         });
+        console.log('Ready to receive requests')
     } catch (error) {
         console.error(error)
     }
 
-    // Get just the string response
-    const output_json = response.data.choices
-    const output = output_json[0].message.content
-    console.log('M: ' + output)
-    console.log('')
-
-    // Save messages to CSV for long term memory
-    saveMessagesToCSV(selectedPokemon.NationalPokedexNumber)
 }
 
 // Send a message to the Pokemon with message array as well
@@ -101,14 +105,20 @@ async function sendChatToPokemon(prompt) {
 
     // ChatGPT Response
     let response
-    // Save prompt as user response to messages array
-    messageLogs[selectedPokemon.NationalPokedexNumber].push({ role: "user", content: prompt, timestamp: new Date().toISOString() })
+    // Save prompt as user response to runningMemoryLogs and interactionHistoryLogs array
+    runningMemoryLogs[selectedPokemon.NationalPokedexNumber].push({ role: "user", content: prompt, timestamp: new Date().toISOString() });
+    interactionHistoryLogs[selectedPokemon.NationalPokedexNumber].push({ role: "user", content: prompt, timestamp: new Date().toISOString() });
 
     // Send user response with previous message array
     try {
+        // Log the entire primeRunningMemory array
+        console.log("primeRunningMemory:");
+        runningMemoryLogs[selectedPokemon.NationalPokedexNumber].forEach((message, index) => {
+            console.log(`Message ${index + 1}:`, message);
+        });
         response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
-            messages: messageLogs[selectedPokemon.NationalPokedexNumber].map(({ role, content }) => ({ role, content })), // Only send messages for this Pokemon
+            messages: runningMemoryLogs[selectedPokemon.NationalPokedexNumber].map(({ role, content }) => ({ role, content })), // Only send messages for this Pokemon
             temperature: 0.7,
             max_tokens: 100,
         });
@@ -119,8 +129,9 @@ async function sendChatToPokemon(prompt) {
     // Get just the string response
     const output_json = response.data.choices
     const output = output_json[0].message.content
-    // Save ChatGPT's response to message array
-    messageLogs[selectedPokemon.NationalPokedexNumber].push({ role: "system", content: output, timestamp: new Date().toISOString() })
+    // Save ChatGPT's response to runningMemoryLogs and interactionHistoryLogs array
+    runningMemoryLogs[selectedPokemon.NationalPokedexNumber].push({ role: "system", content: output, timestamp: new Date().toISOString() });
+    interactionHistoryLogs[selectedPokemon.NationalPokedexNumber].push({ role: "system", content: output, timestamp: new Date().toISOString() });
     console.log('M ' + output)
 
     // Save messages to CSV
@@ -144,7 +155,7 @@ function saveMessagesToCSV(pokedexNumber) {
         append: true, // Add this line to append new messages to the existing ones
     });
 
-    csvWriter.writeRecords(messageLogs[pokedexNumber])
+    csvWriter.writeRecords(interactionHistoryLogs[pokedexNumber])
         .then(() => {
             console.log(`Messages saved to CSV file for Pokemon #${pokedexNumber}`);
         })
@@ -157,24 +168,27 @@ function saveMessagesToCSV(pokedexNumber) {
 async function loadMessagesFromCSV(pokedexNumber) {
     const csv = require('csv-parser');
     const results = [];
-  
+
     try {
-      const csvPath = path.join(directoryPath, `messages_${pokedexNumber}.csv`);
-      const csvExists = fs.existsSync(csvPath);
-  
-      if (csvExists) {
-        const csvStream = fs.createReadStream(csvPath).pipe(csv({ headers: ['role', 'content', 'timestamp'] }));
-        csvStream.on('data', (data) => results.push(data));
-        csvStream.on('error', (error) => console.error(`Error loading messages for Pokemon #${pokedexNumber} from CSV file:`, error));
-        await once(csvStream, 'end');
-      }
-  
-      messageLogs[pokedexNumber] = results.filter(isMessageWithinDuration);
-      console.log(`Messages for Pokemon #${pokedexNumber} loaded from CSV file`);
+        const csvPath = path.join(directoryPath, `interaction_history_${pokedexNumber}.csv`);
+        const csvExists = fs.existsSync(csvPath);
+
+        if (csvExists) {
+            const csvStream = fs.createReadStream(csvPath).pipe(csv({ headers: ['role', 'content', 'timestamp'] }));
+            csvStream.on('data', (data) => results.push(data));
+            csvStream.on('error', (error) => console.error(`Error loading interaction history for Pokemon #${pokedexNumber} from CSV file:`, error));
+            await once(csvStream, 'end');
+        }
+
+        interactionHistoryLogs[pokedexNumber] = results.filter(isMessageWithinDuration);
+        console.log(`Interaction history for Pokemon #${pokedexNumber} loaded from CSV file`);
+
+        // Add interaction history to running memory
+        runningMemoryLogs[pokedexNumber] = interactionHistoryLogs[pokedexNumber].slice();
     } catch (error) {
-      console.error(`Error loading messages for Pokemon #${pokedexNumber} from CSV file:`, error);
+        console.error(`Error loading interaction history for Pokemon #${pokedexNumber} from CSV file:`, error);
     }
-  }
+}
 
 // Check if a message is within the specified duration
 function isMessageWithinDuration(message) {
