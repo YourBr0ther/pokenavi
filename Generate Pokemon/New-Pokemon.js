5bf41c3cfdf2bb6d6d64ec6779b37b23278a1ad5
YourBrother <yourbr0ther.tv@gmail.com>
2023-04-25 22:59:59 -0400

    Successfully created a JS script for creating Generic pokemon

--- Generate Pokemon/New-Pokemon.js
+++ Generate Pokemon/New-Pokemon.js
@@ -1,77 +1,119 @@
-// This is the Pokemon Creator script. It will be used to create Generic Pokemon to be used within PokeNavi
-
 // Modules
-import inquirer from 'inquirer';
-const { create } = require('domain')
-const readline = require('readline')
+const readline = require('readline');
+const fs = require('fs');
 
-// Answer's array
-let answerArray = {
+// Create a readline interface for input/output
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-    user_name: "<$trainerName>",
-    user_gender: "<$trainerGender>",
-    systen_interest: "$hobby" ,
-    system_description : ["<$entry1>","<$entry2>","<$entry3>","<$entry4>","<$entry5>"],
-    system_age : "<$age>",
-    system_personality: "$traits",
-    system_species: "<$species>",
-    system_name: "<$nickname>",
-    system_gender: "<$genderfemale>"
-
+function removeNewlines(text) {
+  return text.replace(/\n|\f/g, ' ');
 }
 
-const questions = [];
+async function getPokemonEntries(species, count = 5) {
+  try {
+    const fetch = (await import('node-fetch')).default;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${species.toLowerCase()}`);
+    const speciesData = await response.json();
+    const pokemonResponse = await fetch(speciesData.varieties[0].pokemon.url);
+    const pokemon = await pokemonResponse.json();
 
-for (const key in answerArray) {
-  if (answerArray.hasOwnProperty(key)) {
-    const question = {
-      type: 'input',
-      name: key,
-      message: `Please enter your ${key.replace(/_/g, ' ')}:`,
-      default: answerArray[key]
-    };
-    questions.push(question);
+    const entries = speciesData.flavor_text_entries
+      .slice(0, count)
+      .map((entry) => removeNewlines(entry.flavor_text));
+
+    return entries;
+  } catch (error) {
+    console.error('Error fetching Pokémon entries:', error.message);
+    return [];
   }
 }
 
-inquirer
-  .prompt(questions)
-  .then((answers) => {
-    answerArray = Object.assign(answerArray, answers);
+// Answer's array
+let answerArray = {
+  user_name: "<$trainerName>",
+  user_gender: "<$trainerGender>",
+  system_interest: "<$hobby>",
+  system_age: "<$age>",
+  system_personality: "<$traits>",
+  system_species: "<$species>",
+  system_name: "<$nickname>",
+  system_gender: "<$genderfemale>"
+};
+
+const promptQuestion = (question) => {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+};
+
+// Update the promptUser function to fetch the Pokémon entries
+async function promptUser() {
+  for (const key in answerArray) {
+    if (answerArray.hasOwnProperty(key)) {
+      const question = `Please enter your ${key.replace(/_/g, ' ')}: `;
+      const answer = await promptQuestion(question);
+      answerArray[key] = answer;
+    }
+  }
+
+  answerArray.system_description = await getPokemonEntries(answerArray.system_species);
+
+  rl.close();
+
+  // Call createPokemon and writeFile after user input is collected
+  const pokemon = createPokemon(template, answerArray);
+
+  // Write the pokemon object to a JSON file
+  fs.writeFile('pokemon.json', JSON.stringify(pokemon), (err) => {
+    if (err) throw err;
+    console.log('Pokemon saved!');
   });
 
-// Pokemon template
-const template = {
+  console.log(pokemon);
+}
 
-    response : "Response text based on the user input",
-    memory : ["key-value store of things I should remember about myself and the user"],
-    topics_to_explore: ["detailed description of interesting topics to explore further"],
-    previous_conversation_points: ["list of previous conversation points"],
-    current_topic: "detailed description of the current topic",
-    dreams: "Create simulated detailed description of the system's dreams based on previous conversations",
-    inner_dialogue: "Create simulated inner_dialogue based on the conversation",
-    private_thoughts: "Create simulated private thoughts based on the conversation",
-    user_name: "<$trainerName>",
-    user_gender: "<$trainerGender>",
-    systen_interest: "$hobby" ,
-    system_description : ["<$entry1>","<$entry2>","<$entry3>","<$entry4>","<$entry5>"],
-    system_age : "<$age>",
-    system_personality: "$traits",
-    system_species: "<$species>",
-    system_name: "<$nickname>",
-    system_gender: "<$genderfemale>"
+promptUser();
+
+// Update the template object
+const template = {
+  response: "Response text based on the user input",
+  memory: ["key-value store of things I should remember about myself and the user"],
+  topics_to_explore: ["detailed description of interesting topics to explore further"],
+  previous_conversation_points: ["list of previous conversation points"],
+  current_topic: "detailed description of the current topic",
+  dreams: "Create simulated detailed description of the system's dreams based on previous conversations",
+  inner_dialogue: "Create simulated inner_dialogue based on the conversation",
+  private_thoughts: "Create simulated private thoughts based on the conversation",
+  user_name: "<$user_name>",
+  user_gender: "<$user_gender>",
+  system_interest: "<$system_interest>",
+  system_description: ["<$system_description[0]>", "<$system_description[1]>", "<$system_description[2]>", "<$system_description[3]>", "<$system_description[4]>"],
+  system_age: "<$system_age>",
+  system_personality: "<$system_personality>",
+  system_species: "<$system_species>",
+  system_name: "<$system_name>",
+  system_gender: "<$system_gender>"
 };
 
 function createPokemon(template, answerArray) {
-    for (const key in answerArray) {
-        if (template.hasOwnProperty(key)) {
-            template[key] = answerArray[key]
-        }
+  const myPokemon = {};
+
+  for (const key in template) {
+    if (template.hasOwnProperty(key)) {
+      if (key === 'system_description') {
+        myPokemon[key] = answerArray[key];
+      } else if (typeof template[key] === 'string') {
+        myPokemon[key] = template[key].replace(/<\$([a-zA-Z0-9_]+)>/g, (_, matchedKey) => answerArray[matchedKey]);
+      } else {
+        myPokemon[key] = template[key];
+      }
     }
+  }
 
-    return myPokemon
+  return myPokemon;
 }
-
-let pokemon = createPokemon()
-console.log(pokemon)
-
