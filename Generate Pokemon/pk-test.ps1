0f001da8e2ef0106b0bcb161be8a6e11d3876200
YourBrother <yourbr0ther.tv@gmail.com>
2023-05-01 00:06:03 -0400

    cleaned up DATA block

--- Generate Pokemon/pk-test.ps1
+++ Generate Pokemon/pk-test.ps1
@@ -4,14 +4,11 @@ function Floor([double]$number) {
     [Math]::Floor($number)
 }
 
-# $pokemonExport = ".\Generate Pokemon\Sample PK3\CHARMELEON.pk3" 
-# $pk3Data = [System.IO.File]::ReadAllBytes($pokemonExport)
-
 $hexString = "9de847ffe1dd6e3bbdbbcdbdc9c9c8ff80430202c5d9e2ffffff00a4f100007c3529c47c3529c47c3529c4593429c4013529c47c7329c47c0eace45875f8c97c3529c4163529c47c3529c4623529c4"
 $pk3Data = [System.Byte[]]::new($hexString.Length / 2)
- for ($i = 0; $i -lt $hexString.Length; $i += 2) {
-     $pk3Data[$i / 2] = [System.Byte]::Parse($hexString.Substring($i, 2), [System.Globalization.NumberStyles]::HexNumber)
- }
+for ($i = 0; $i -lt $hexString.Length; $i += 2) {
+    $pk3Data[$i / 2] = [System.Byte]::Parse($hexString.Substring($i, 2), [System.Globalization.NumberStyles]::HexNumber)
+}
 
 $pokemonID = $pk3Data[3..0]
 $reversePokemonID = $pk3Data[0..3]
@@ -25,11 +22,9 @@ Write-Host ""
 $decimalValue = [System.UInt32]::Parse($hexPokemonID, [System.Globalization.NumberStyles]::HexNumber)
 Write-Host "The decimal value of $hexPokemonID is $decimalValue"
 
-# Calculate ABCDOrder
 $ABCDOrder = Floor($decimalValue % 24)
 $ABCDStructure = @("ABCD", "ABDC", "ACBD", "ACDB", "ADBC", "ADCB", "BACD", "BADC", "BCAD", "BCDA", "BDAC", "BDCA", "CADB", "CBAD", "CBDA", "CDAB", "CDBA", "DABC", "DACB", "DBAC", "DBCA", "DCAB", "DCBA")
-$ABCDOrderValue = $ABCDStructure[$ABCDOrder - 1]  # Get the ABCD order value from the ABCDStructure array
-
+$ABCDOrderValue = $ABCDStructure[$ABCDOrder - 1]
 Write-Host ""
 Write-Host "ABCDOrder: $ABCDOrderValue"
 
@@ -54,7 +49,6 @@ for ($i = 31; $i -le 78; $i += 12) {
         }
         $blocks += $block
         $reversedBlocks += $reversedBlock
-
         $decryptedBlock = [Convert]::ToString(([Convert]::ToInt32($reversedBlock, 16) -bxor [Convert]::ToInt32($hexOXR, 16)), 16)
         $decryptedBlocks += $decryptedBlock
     }
@@ -64,8 +58,5 @@ for ($i = 31; $i -le 78; $i += 12) {
         Write-Host "REV HEX RAW Block [$(($i + $j * 4 + 3)..($i + $j * 4))]: $($reversedBlocks[$j])"
         Write-Host "HEX Decrypted Block: $($decryptedBlocks[$j])"
     }
-
-    $combined = $decryptedBlocks -join ""
-    Write-Host "HEX Block: $combined"
     Write-Host ""
 }
