2efb7f2467c1d9cc11c93107ebed0b92a40b8e06
Chris Vance <yourbr0ther.tv@gmail.com>
2023-05-03 12:10:34 -0400

    Successfully pulled the data blocks into separate strings identified by their group name

--- Generate Pokemon/Import-Pokemon-v2.ps1
+++ Generate Pokemon/Import-Pokemon-v2.ps1
@@ -1,7 +1,7 @@
 Clear-Host
 
 # Variables
-#$pokemonPath = ".\Generate Pokemon\Sample PK3\CHARMELEON.pk3"
+$pokemonPath = ".\Generate Pokemon\Sample PK3\CHARMELEON.pk3"
 $Mappings = ".\Generate Pokemon\Mappings"
 
 function Get-PokemonBytes {
@@ -10,7 +10,7 @@ function Get-PokemonBytes {
         [String]
         $Path
     )
-    $pk3Data = $([System.IO.File]::ReadAllBytes($pokemonExport))[0..78]
+    $pk3Data = $([System.IO.File]::ReadAllBytes($Path))[0..78]
     return $pk3Data
 }
 
@@ -168,20 +168,86 @@ function Get-Markings([int]$byte27) {
 
 }
 
-#$pokemonHEX = "9DE847FFE1DD6E3BBDBBCDBDC9C9C8FF80430202C5D9E2FFFFFF00A4F100007C3529C47C3529C47C3529C4593429C4013529C47C7329C47C0EACE45875F8C97C3529C4163529C47C3529C4623529C4"
-$pokemonHEX = "8F11F92D198BF0A6CAE9E2D7DCEDFF0807000202BDC2CCC3CDFFFF00370700003800000084010000006500000A002B0043000000231E1400000001000000000000000000007A042247A8803D000000"
+function Get-abcdDATA ([string]$ABCDOrder) {
+
+    for ($h = 0; $h -le 4; $h++) {
+        $dataStructure = ""
+
+        for ($i = 31; $i -le 78; $i += 12) {
+            $blocks = @()
+            $reversedBlocks = @()
+            $decryptedBlocks = @()
+    
+            for ($j = 0; $j -lt 3; $j++) {
+                $block = ""
+                $reversedBlock = ""
+                for ($k = 0; $k -lt 4; $k++) {
+                    $block += $pk3Data[$i + $j * 4 + $k].ToString("X2")
+                    $reversedBlock += $pk3Data[$i + $j * 4 + 3 - $k].ToString("X2")
+                }
+                $blocks += $block
+                $reversedBlocks += $reversedBlock
+                $decryptedBlock = [Convert]::ToString(([Convert]::ToInt32($reversedBlock, 16) -bxor [Convert]::ToInt32($hexOXR, 16)), 16)
+                $decryptedBlocks += $decryptedBlock.PadLeft(8, '0')
+
+            }
+
+            $dataStructure += $decryptedBlocks
+
+        }
+
+    }
+
+    $dataStructure = $dataStructure.Replace(" ", "")
+    $tempDataArray = @{
+        A = $dataStructure[0..23] -join ""
+        B = $dataStructure[24..47] -join ""
+        C = $dataStructure[48..71] -join ""
+        D = $dataStructure[72..96] -join ""
+    }
+
+    foreach ($letter in [char[]]$ABCDOrder) {
+
+        switch ($letter) {
+            "A" { $tempGrowth = $tempDataArray."$letter" }
+            "B" { $tempMoves = $tempDataArray."$letter" }
+            "C" { $tempEVs = $tempDataArray."$letter" }
+            "D" { $tempMisc = $tempDataArray."$letter" }
+        }
+
+    }
+
+    
+    $DataArray = @{
+        "Growth" = $tempGrowth
+        "Moves"  = $tempMoves
+        "EVs"    = $tempEVs
+        "Misc"   = $tempMisc
+    }
+        
+    return $DataArray
+
+}
+
+# Test
+$pokemonHEX = "9DE847FFE1DD6E3BBDBBCDBDC9C9C8FF80430202C5D9E2FFFFFF00A4F100007C3529C47C3529C47C3529C4593429C4013529C47C7329C47C0EACE45875F8C97C3529C4163529C47C3529C4623529C4"
+# Mankey
+#$pokemonHEX = "8F11F92D198BF0A6CAE9E2D7DCEDFF0807000202BDC2CCC3CDFFFF00370700003800000084010000006500000A002B0043000000231E1400000001000000000000000000007A042247A8803D000000"
 $pk3Data = [byte[]]::new($pokemonHEX.Length / 2)
 for ($i = 0; $i -lt $pokemonHEX.Length; $i += 2) {
     $pk3Data[$i / 2] = [convert]::ToByte($pokemonHEX.Substring($i, 2), 16)
 }
 
+#$pk3Data = Get-PokemonBytes -Path $pokemonPath
 $reversePokemonID = $pokemonHEX[0..7] -join ""
 $pokemonIDBytes = $pk3Data[3..0]
+$PokemonIDHex = ($pk3Data[3..0] | ForEach-Object { $_.ToString("X2") }) -join ""
 $normalPokemonID = ($pokemonIDBytes | ForEach-Object { $_.ToString("X2") }) -join ""
 $TrainerIDHex = ($pk3Data[7..4] | ForEach-Object { "{0:X2}" -f $_ }) -join ""
 $TrainerID = Get-TrainerID $pk3Data[4..7]
 $secretTrainerIDHex = ($pk3Data[7..6] | ForEach-Object { "{0:X2}" -f $_ }) -join ""
 $secretTrainerID = Get-SecretTrainerID $pk3Data[4..7]
+$hexOXR = [Convert]::ToString(([Convert]::ToInt32($TrainerIDHex, 16) -bxor [Convert]::ToInt32($PokemonIDHex, 16)), 16)
 $nature = Get-Nature -PokemonID $normalPokemonID
 #$gender = Get-Gender -PokemonID $normalPokemonID
 $ABCDOrder = Get-ABCDOrder -PokemonID $normalPokemonID
@@ -190,15 +256,20 @@ $isShiny = Get-ShinyStatus -decryptionKey $decryptionKey
 $pokemonName = Get-Name -nameBytes $pk3Data[8..19]
 $trainerName = Get-Name -nameBytes $pk3Data[20..26]
 $markings = Get-Markings -markingByte $pk3Data[27]
+$growth = $(Get-abcdDATA -ABCDOrder $ABCDOrder)."Growth"
+$moves = $(Get-abcdDATA -ABCDOrder $ABCDOrder)."Moves"
+$evs = $(Get-abcdDATA -ABCDOrder $ABCDOrder)."EVs"
+$misc = $(Get-abcdDATA -ABCDOrder $ABCDOrder)."Misc"
 
 Write-Host ""
 Write-Host "PokemonHEX: $pokemonHEX"
 Write-Host "PokemonID - [R]: $reversePokemonID"
 Write-Host "PokemonID - [N]: $normalPokemonID"
+Write-Host "PokemonID - [H]: $PokemonIDHex"
 Write-Host "Nature: $nature"
-#Write-Host "Gender: $gender"
+#Write-Host "Gender: $gender" # this will get enabled via the species number
 Write-Host "ABCD Order: $ABCDOrder"
-Write-Host "TrainerID - H]: $TrainerIDHex" 
+Write-Host "TrainerID - [H]: $TrainerIDHex" 
 Write-Host "TrainerID - [N]: $TrainerID"
 Write-Host "Secret TrainerID - [H]: $secretTrainerIDHex"
 Write-Host "Secret TrainerID - [N]: $secretTrainerID"
@@ -207,3 +278,7 @@ Write-Host "Shiny Status: $isShiny"
 Write-Host "Pokemon Name: $pokemonName"
 Write-Host "Trainer Name: $trainerName"
 Write-Host "Markings: $markings"
+Write-Host "Growth: $growth"
+Write-Host "Moves: $moves"
+Write-Host "EVs: $evs"
+Write-Host "Misc: $misc"
