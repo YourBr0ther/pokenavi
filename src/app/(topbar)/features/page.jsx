101598ce5c568fa98fc8b07390a3be10fefc5eb6
Chris Vance <cvance@highlandsmortgage.com>
2023-06-06 16:55:36 -0400

    added css pages for each page

--- src/app/(topbar)/features/page.jsx
+++ src/app/(topbar)/features/page.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './page.module.css'
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className={styles.container}>Features</div>
   )
 }
 
--- src/app/(topbar)/getpokenavi/page.jsx
+++ src/app/(topbar)/getpokenavi/page.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './page.module.css'
 
 const GetPokeNavi = () => {
   return (
-    <div>Get PokeNavi</div>
+    <div className={styles.container}>Get PokeNavi</div>
   )
 }
 
