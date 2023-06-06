eadd23b2d60d1d7227b4088e7ba51f3ba792213e
Chris Vance <cvance@highlandsmortgage.com>
2023-06-06 15:47:30 -0400

    Added header, footer, and navbar

---
+++ src/app/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <div>
+        <Link href="/">PokeNavi</Link>
+        
+        </div>
+  )
+}
+
+export default Navbar
