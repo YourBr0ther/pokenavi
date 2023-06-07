8d993e125201c684fa2083ca9eeba9eb672503f3
Chris Vance <cvance@highlandsmortgage.com>
2023-06-07 14:13:17 -0400

    created a basic features page

--- src/app/(topbar)/features/page.jsx
+++ src/app/(topbar)/features/page.jsx
@@ -1,9 +1,87 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from "next/image"
 
 const Features = () => {
   return (
-    <div className={styles.container}>Features</div>
+    <div className={styles.container}>
+
+      <div className={styles.feature}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Extend the adventure</h1>
+          <p className={styles.desc}>There is no reason to stop enjoying the Pokemon that you battled with after so many encounters. Why not bring them with you into the real world.</p>
+        </div>
+        <div className={styles.imgborder}>
+          <Image className={styles.img} src="/feature.png" alt="Feature 1" width={175} height={175}></Image>
+        </div>
+      </div>
+
+      <div className={styles.feature}>
+      <div className={styles.imgborder}>
+          <Image className={styles.img} src="/feature.png" alt="Feature 1" width={175} height={175}></Image>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Extend the adventure</h1>
+          <p className={styles.desc}>There is no reason to stop enjoying the Pokemon that you battled with after so many encounters. Why not bring them with you into the real world.</p>
+        </div>
+      </div>
+      <div className={styles.feature}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Extend the adventure</h1>
+          <p className={styles.desc}>There is no reason to stop enjoying the Pokemon that you battled with after so many encounters. Why not bring them with you into the real world.</p>
+        </div>
+        <div className={styles.imgborder}>
+          <Image className={styles.img} src="/feature.png" alt="Feature 1" width={175} height={175}></Image>
+        </div>
+      </div>
+
+      <div className={styles.feature}>
+      <div className={styles.imgborder}>
+          <Image className={styles.img} src="/feature.png" alt="Feature 1" width={175} height={175}></Image>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Extend the adventure</h1>
+          <p className={styles.desc}>There is no reason to stop enjoying the Pokemon that you battled with after so many encounters. Why not bring them with you into the real world.</p>
+        </div>
+      </div>      <div className={styles.feature}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Extend the adventure</h1>
+          <p className={styles.desc}>There is no reason to stop enjoying the Pokemon that you battled with after so many encounters. Why not bring them with you into the real world.</p>
+        </div>
+        <div className={styles.imgborder}>
+          <Image className={styles.img} src="/feature.png" alt="Feature 1" width={175} height={175}></Image>
+        </div>
+      </div>
+
+      <div className={styles.feature}>
+      <div className={styles.imgborder}>
+          <Image className={styles.img} src="/feature.png" alt="Feature 1" width={175} height={175}></Image>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Extend the adventure</h1>
+          <p className={styles.desc}>There is no reason to stop enjoying the Pokemon that you battled with after so many encounters. Why not bring them with you into the real world.</p>
+        </div>
+      </div>      <div className={styles.feature}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Extend the adventure</h1>
+          <p className={styles.desc}>There is no reason to stop enjoying the Pokemon that you battled with after so many encounters. Why not bring them with you into the real world.</p>
+        </div>
+        <div className={styles.imgborder}>
+          <Image className={styles.img} src="/feature.png" alt="Feature 1" width={175} height={175}></Image>
+        </div>
+      </div>
+
+      <div className={styles.feature}>
+      <div className={styles.imgborder}>
+          <Image className={styles.img} src="/feature.png" alt="Feature 1" width={175} height={175}></Image>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Extend the adventure</h1>
+          <p className={styles.desc}>There is no reason to stop enjoying the Pokemon that you battled with after so many encounters. Why not bring them with you into the real world.</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.item}>
          <h1 className={styles.title}>Continue the adventure together.</h1>
          <p className={styles.desc}>PokeNavi, a new way to interact with your favorite Pokemon using artifical intelligence to bring them to life.</p>
-         <button className={styles.signup}>Sign Up</button>
+         <button href="/getpokenavi" className={styles.signup}>Sign Up</button>
       </div>
       <div className={styles.item}>
         <Image className={styles.img} src="/homepage.gif" alt="a lovely beach paradise" width={430} height={240}></Image>
