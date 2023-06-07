08c031521b41d3f9f7c251657b349427ab9206a2
Chris Vance <cvance@highlandsmortgage.com>
2023-06-07 16:15:13 -0400

    added an account creation page

--- src/app/(topbar)/getpokenavi/page.jsx
+++ src/app/(topbar)/getpokenavi/page.jsx
@@ -3,7 +3,21 @@ import styles from './page.module.css'
 
 const GetPokeNavi = () => {
   return (
-    <div className={styles.container}>Get PokeNavi</div>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.title}>Create an Account</div>
+        <div className={styles.content}>
+          <form className={styles.form}>
+            <input type="Username" placeholder="Username" className={styles.input}/>
+            <input type="Email Address" placeholder="Email Address" className={styles.input}/>
+            <input type="Display Name" placeholder="Display Name" className={styles.input}/>
+            <input type="Age" placeholder="Age" className={styles.input}/>
+            <input type="Gender" placeholder="Gender" className={styles.input}/>
+          </form>
+        </div>
+        <button className={styles.submit}>Submit</button>
+      </div>
+    </div>
   )
 }
 
