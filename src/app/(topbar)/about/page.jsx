cfac61aadd90ed4d769592e1ee1ad3cb3fd3b3cf
Chris Vance <cvance@highlandsmortgage.com>
2023-06-07 14:59:35 -0400

    created a basic about page

--- src/app/(topbar)/about/page.jsx
+++ src/app/(topbar)/about/page.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from "next/image"
 
 const About = () => {
   return (
-    <div className={styles.container}>About</div>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <div className={styles.title}>
+            About
+          </div>
+          <div className={styles.desc}>
+            Here at PokeNavi, we seek to foster new friendships and rekindle the old flames that so many Pokemon created in our youth.
+          </div>
+        </div>
+        <Image className={styles.img} src="/about.png" alt="about picture" width={720} height={405}></Image>
+      </div>
+    </div>
   )
 }
 
