c3cc583e62322faba4885e28be1693d3f8262f97
Chris Vance <cvance@highlandsmortgage.com>
2023-06-07 16:40:41 -0400

    started working on schemas for mongo

---
+++ src/models/Users.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose.Schema;
+
+const UserSchema = new Schema({
+  displayname: {type: String, required: true},
+  emailaddress: {type: String, required: true, unique: true},
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  securityQuestion: { type: String, required: true },
+  securityAnswer: { type: String, required: true },
+});
+
+export default mongoose.model("User", UserSchema)
---
+++ src/utils/db.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+import mongoose from "mongoose";
+
+const connect = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGODB_SERVER);
+
+    } catch (error) {
+        throw new Error("connection failed!")
+    }
+}
+
+export default connect;
