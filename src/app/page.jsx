d6994b07e1bd9a7d47da2563fb61ae4f9d005bb3
Chris Vance <cvance@highlandsmortgage.com>
2023-06-07 11:27:03 -0400

    started working on homepage divs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
-import styles from './globals.css'
+import styles from './page.module.css'
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div>PokeNavi</div>
-      <div>A new way to interact with your team</div>
+      <div className={styles.item}>
+         <div>Continue the Adventure with your favorite sidekicks</div>
+         <div>PokeNavi, a new way to interact with your favorite Pokemon using artifical intelligence to bring them to life.</div>
+         <button className={styles.button}> Sign Up </button>
       </div>
+      <div className={styles.item}>
+        <Image className={styles.img} src="" alt="homepage.png"></Image>
+      </div>
+    </div>
   )
 }
