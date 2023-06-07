f435a6dc1c0dcbe5ace08edc39910ed99eba81b7
Chris Vance <cvance@highlandsmortgage.com>
2023-06-07 10:45:24 -0400

    added footer with icons and separated the body data on the layout

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import styles from "./footer.module.css"
+import Image from 'next/image'
+
 
 const Footer = () => {
       return (
             <div>
-            <div>©2023 PokeNavi. All rights reserved</div>
-            </div>
+                  <div className={styles.footerbar}>
+                        <div><Image src="/poke-radar-512.png" alt="PokeRadar" width={32} height={32}></Image>©2023 PokeNavi. All rights reserved</div>
+                        <div className={styles.social}>
+                              <Image src="/1.png" alt="PokeNavi Facebook" width={32} height={32}></Image>
+                              <Image src="/2.png" alt="PokeNavi Instagram" width={32} height={32}></Image>
+                              <Image src="/3.png" alt="PokeNavi Twitter" width={32} height={32}></Image>
+                              <Image src="/4.png" alt="PokeNavi YouTube" width={32} height={32}></Image>
+                        </div >
+                  </div>
+            </div >
       )
 
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,9 @@ import styles from './globals.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>PokeNavi | A new way to interact with your team</div>
+    <div className={styles.container}>
+      <div>PokeNavi</div>
+      <div>A new way to interact with your team</div>
+      </div>
   )
 }
